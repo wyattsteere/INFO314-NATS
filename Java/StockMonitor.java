f2c55e30453848bb03fb959175623e700447bd81
Ja-sonny <jasonny460@gmail.com>
2023-05-21 21:34:33 -0700

    StockMonitor by Jason

--- Java/StockMonitor.java
+++ Java/StockMonitor.java
@@ -3,48 +3,80 @@ import io.nats.client.*;
 import java.io.*;
 import java.sql.SQLOutput;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.*;
+import org.xml.sax.InputSource;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+
 public class StockMonitor {
+    private static ArrayList<String> symbols = new ArrayList<String>();
+    public StockMonitor(String... args) {
+        for(int i = 0; i < args.length; i++) {
+            symbols.add(args[i]);
+        }
+    }
     public static void main(String[] args) throws Exception {
-        String asdf = "";
-        String logFilePath = ""; //path to log file 
-        Connection nc = Nats.connect("nats://localhost:4222");
-        NatsMessageLogger logger = new NatsMessageLogger(logFilePath);
+        StockMonitor sm = new StockMonitor(args);
+        sm.start();
+    }
+    public static void start() {
+        try {
+            Connection nc = Nats.connect("nats://localhost:4222");
+            Dispatcher d = nc.createDispatcher((msg) -> {
+                String symbol = msg.getSubject();
+                String message = new String(msg.getData());
 
-        Dispatcher dispatchNAS = nc.createDispatcher((msg) -> {
-            System.out.println("NASDAQ stock prices: " + new String(msg.getData()));
-        });
-        dispatchNAS.subscribe("NASDAQ"); //subscribing to entire stock market
+                helper(symbol, message);
+            });
+            if (symbols.size() < 1){
+                d.subscribe(">");
+            } else {
+                for(String symbol:symbols) {
+                    d.subscribe(symbol);
+                }
+            }
+            
+            while (true) {
+                Thread.sleep(500);
+            }
+        } catch(Exception e) {
 
-        Dispatcher dispatchDOW = nc.createDispatcher((msg) -> {
-            asdf = ("DOWJones stock prices: " + new String(msg.getData())); //save as var
-        });
-        dispatchDOW.subscribe("DOWJones"); //example names
+        }
+    }
 
-        logger.logMessage(asdf);
+    public static void helper(String symbol, String message){
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
 
-        // Close the logger and connection
-        logger.close();
-        connection.close();
+            Document doc = builder.parse(new InputSource(new StringReader(message)));
+            Element root = doc.getDocumentElement();
+            String price = root.getElementsByTagName("adjustedPrice").item(0).getTextContent();
+            String change = root.getElementsByTagName("adjustment").item(0).getTextContent();
+            String[] date = message.split("\"");
+            String time = date[1];
+            String filename = "logs/" + symbol + ".txt";
+            String data = time + " " + change + " " + price + "\n";
+            File f = new File(filename);
+            if(!f.exists()){
+                f.createNewFile();
+            }
+            FileWriter fw = new FileWriter(filename, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(data);
+            bw.close();
+
+
+        }catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
 
-public class NatsMessageLogger {
-    private BufferedWriter writer;
-
-    public NatsMessageLogger(String logFilePath) throws IOException {
-        writer = new BufferedWriter(new FileWriter(logFilePath, true));
-    }
-
-    public void logMessage(String message) throws IOException {
-        writer.write(message);
-        writer.newLine();
-        writer.flush();
-    }
-
-    public void close() {
-        try {
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
